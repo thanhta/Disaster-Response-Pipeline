7ac471ed8afdb8fb7086bcea3f3d8aa0fb5c8113
Thanh Ta <tta@its.jnj.com>
2022-10-29 21:09:04 -0400

    Update train_classifier.py with codes transferred from project workspace IDE.  classifier.pkl file is close to 1 GB files. Thus I do not  push to Github

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,140 @@
-import sys
+import sys# import libraries
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
 
+import re
+import numpy as np
+import pandas as pd
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sqlalchemy import create_engine
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import classification_report
+import pickle 
 
 def load_data(database_filepath):
-    pass
-
+    '''
+    Function:
+        1.Load the message data from the Database
+        2.Generate Feature Dataframes of features from message data, Target Dataframes of multiple category labels and category_names
+    
+    Args:
+        database_filepath (str): filepath of the database
+        
+    Returns:        
+        1.X(DataFrame): Feature Dataframes of features from message data
+        2.Y(DataFrame): Target Dataframes of multiple category labels
+        3.category_names(list of str): category labels
+    '''
+    
+    # load data from database
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    df = pd.read_sql_table('DisasterResponse',engine)
+    
+    X = df['message']
+    Y = df[df.columns[4:]]
+    category_names = Y.columns.tolist()
+    return X,Y,category_names
 
 def tokenize(text):
-    pass
+    """
+    Function: split text into words, convert to lowercase and return the root form of the words
+    
+    Args:
+      text(str): the message
+      
+    Return:
+      process_token (list of str): a list of the root form of the message words
+    """
+    
+    # Tokenize text into words
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
 
+    # Lemmatization and lowercase
+    process_tokens = []
+    for tok in tokens:
+        process_tok = lemmatizer.lemmatize(tok).lower().strip()
+        process_tokens.append(process_tok)
+
+    return process_tokens
+    
 
 def build_model():
-    pass
+    """
+     Function: build a model for classifing the disaster messages after applying model pipeline and GridSearch to find the optimum model parameters
+     
+     Args: none
+     
+     Return:
+       cv: an "optimum" classification model after applying 
+     """
+    
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer = tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf',  MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    parameters = {
+        'clf__estimator__n_estimators':[50,100]
+    }
+    
+    cv = GridSearchCV(pipeline, param_grid=parameters, cv=2, n_jobs=-1, verbose=3)
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
-
+    '''
+    Function: Print the average accuracy of the model and performance matrix of the model for all the different categories based on the classification_report
+    
+    Args:
+        model               : Model is used for the predictions 
+        X_test (Data Frame) : Feature Data Frames to be used for predictions
+        Y_test (Data Frame) : Target Data Frames which are Values of multiple labels
+        category_names (list of str): Name of Target Columns
+        
+    Returns:
+        None
+    '''
+    
+    # Testing the model and Predict the vakue 
+    Y_pred = model.predict(X_test)
+    
+    # Calculate the model's average accuracy
+    accuracy = (Y_pred == Y_test.values).mean()
+    print('The model average accuracy is {:.3f}'.format(accuracy))
+    print('*'*60)
+    print('\n')
+    
+    # Printing the classification report for each label
+    idx = -1
+    for col in category_names:
+        idx = idx + 1
+        print('Feature {}: {}'.format(idx, col))
+        print(classification_report(Y_test[col], Y_pred[:, idx]))      
+    
 
 def save_model(model, model_filepath):
-    pass
+    """
+    Function: Save the model as a pickle file
+    
+    Args:
+    model: the classification model
+    model_filepath (str): the path of model's pickle file
+    
+    Returns:
+        None   
+    """
+    
+    with open(model_filepath, 'wb') as file:
+        pickle.dump(model, file)
 
 
 def main():
